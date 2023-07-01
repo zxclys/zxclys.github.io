70114e226c19eba1a23ef8436421f4916c3c14d7
GitHub <github-actions[bot]@users.noreply.github.com>
2023-07-01 13:23:23 +0800

    Github action update at 2023-07-01 13:23:23.

--- js/img_yyy.js
+++ js/img_yyy.js
@@ -1,7 +1,7 @@
 let imageCurrentNum = 1;
 const imagesPerNum = 50;
 const imagesPageNum = 10;
-const imageMaxCount = 302;
+const imageMaxCount = 357;
 const imageDir = "img/yyy/"
 const imageWidth = 200;
 
